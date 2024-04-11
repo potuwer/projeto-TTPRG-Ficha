b8a338c04598263f21b97907d5af8c8b82f0b9e0
arthur_bizordi <abizordi@gmail.com>
2024-04-11 18:05:21 -0300

    Criaççao do pop-up que recebe o valor da barra, criação das funções que deletam o pop-up e que atualizam a barra conforme o tamanho especificado no pop up

--- Code/javascript/barra.js
+++ Code/javascript/barra.js
@@ -1,14 +1,58 @@
+//Function que deleta a div pai de uma div que é pai de um botao: div > div > botaoX
+function deletar(div) {
+  const divPaiPopUp = div.parentNode;
+  divPaiPopUp.remove();
+}
+
 //Cria um pop-up que pede o valor total da barra e o altera (futuramente imlementa mais utilidades pra barra)
 
 const componentesBarra = document.querySelectorAll(".componente-barra");
 const configsBarra = document.querySelectorAll(".barra button");
 
 configsBarra.forEach((botao) =>
-  botao.addEventListener("click", (e) => {
+  botao.addEventListener("click", () => {
     const barra = botao.parentNode;
-    
-    const popUp =  
-})
+
+    const popUpHTML = `<div class="pop-up-barra">
+    <button class="X"></button>
+    <p>Configurações da barra</p>
+    <hr />
+    <ul>
+      <li>
+        <p>Tamanho da barra:</p>
+
+        <input type="number" />
+      </li>
+      <li><button>OK</button></li>
+    </ul>
+  </div>`;
+
+    const popUp = document.createElement("div");
+    popUp.innerHTML = popUpHTML;
+
+    barra.insertAdjacentElement("afterbegin", popUp);
+
+    const botoes = document.querySelectorAll(".pop-up-barra button");
+
+    botoes.forEach((botao) => {
+      botao.addEventListener("click", () => {
+        const divPopUp = document.querySelector(".pop-up-barra");
+
+        if (botao.className == "X") {
+          deletar(divPopUp);
+        } else {
+          const spans = barra.querySelectorAll("span");
+
+          const inputValor = divPopUp.querySelector("input").value;
+
+          spans.forEach((span) => {
+            span.innerHTML = inputValor;
+          });
+          deletar(divPopUp);
+        }
+      });
+    });
+  })
 );
 
 //Botões que alteram no valor temporario
