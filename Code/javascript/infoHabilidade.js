ea2251b49129ef45c7525cf5378cf02977964a8b
arthur_bizordi <abizordi@gmail.com>
2024-04-16 13:27:00 -0300

    Criação do botao 'ok', ajuste na refatoração do scss do pop-up, inicio da criação da classe de obejtosa habilidade js

--- Code/javascript/infoHabilidade.js
+++ Code/javascript/infoHabilidade.js
@@ -3,10 +3,23 @@ const botaoAddHabilidade = containerHabilidade.querySelector(".add-habilidade");
 
 // Criar objetos(habilidades) e coloca-los numa array
 
-// Criar habilidade e enviar para array
+class Habilidade {
+  nome;
+  foto;
+  tempo;
+  custo;
+  ganho;
+  propriedade;
+  descricao;
+}
+
+const h1 = new Habilidade();
+h1.nome = "Golpe vertical dos ventos";
+
+// Criar habilidade e enviar para array- Caso feche o pop up, abre um alerta pergntando se quers descartar essa habilidade.
 
 // Criar, no container-habilidade, mini habilidade puxando pela array
 
-// Abrir habilidade ja criada, puxando info ela array
+// Abrir habilidade ja criada, puxando info ela array- possivel editar essa array, caso edite, ao fechar, confere-se se ha algm alteração, se houver um alerta pergunta se quer alterar
 
-console.log(botaoAddHabilidade);
+console.log(h1);
