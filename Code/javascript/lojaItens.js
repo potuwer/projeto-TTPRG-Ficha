8591d52cba4d5b29c9e9f371d4f367257c02c8e1
arthur_bizordi <abizordi@gmail.com>
2024-05-08 15:03:45 -0300

    Criação da loja e ajuste

--- Code/javascript/lojaItens.js
+++ Code/javascript/lojaItens.js
@@ -1,4 +1,5 @@
 import { fecharOnFocusOut } from "./utils/focusOut.js";
+import { deletarDiv } from "./utils/botaoX.js"
 
 const containerInventario = document.querySelector(".inventario");
 const btnBuy = containerInventario.querySelector(".item-add")
@@ -9,7 +10,18 @@ btnBuy.addEventListener("click", () => abrirMercado())
 function abrirMercado() {
     const loja = document.createElement("div")
     loja.classList.add("loja")
-    loja.innerHTML = ``
+    loja.innerHTML = `
+    <button class="X X-loja"><img src="./assets/Icons/X.png" /></button>
+    <div class="cabecalho-loja">
+      <p>Itens</p>
+      <div class="carrinho"></div>
+    </div>
+    <hr>
+    <div class="mostruario-loja"></div>
+    <button class="add">ADICIONAR</button>
+    `
     containerInventario.insertAdjacentElement("beforebegin", loja)
     fecharOnFocusOut(loja)
+    const botaoX = loja.querySelector(".X")
+    deletarDiv(botaoX, loja)
 }
